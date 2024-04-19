cd5b9b583cbd30f9be4243b30feb9998a3fbe61b
Soroor Sadat Seyed Aghamiri <aghamiri.soroor@gmail.com>
2024-04-19 16:14:50 -0400

    Crochet page updated

--- obj/Debug/net8.0/MyWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cefea60ca023afae77ba8165627efd37ffb14077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77d1f10e228816b1de8eab1b30c233475a74385")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
