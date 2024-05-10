9a21dab6757525d242f0a080ae5311af2c700c51
Soroor Sadat Seyed Aghamiri <aghamiri.soroor@gmail.com>
2024-05-10 14:30:32 -0400

    Updating software project view

--- Controllers/SoftwarePrjsController.cs
+++ Controllers/SoftwarePrjsController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MyWebApplication.Models;
 using System.Linq;
+using System.Text.Encodings.Web;
 
 
 namespace MyWebApplication.Controllers;
@@ -16,19 +17,28 @@ public class SoftwarePrjs : Controller{
     //     return View(GenerateProjectCards());
     // }
 
-    //Get: /SoftwarePrjs/
-    // Requires using System.Text.Encodings.Web;
-    // public IActionResult Sort(string requestedMode){
-        //convert string to sortmode
-        //sort cards and return view
-    // }
+    public IActionResult Sort(string requestedMode){
+        // Convert string to sortmode enum
+        if (Enum.TryParse(requestedMode, true, out SortModes receivedMode))
+        {
+            // Parsing succeeded, receivedMode contains the parsed enum value
+            Console.WriteLine("Sort mode: " + receivedMode.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Converting unsuccessful");
+            // Parsing failed
+        }
+        return View("~/Views/Home/SoftwarePrjs.cshtml");
+    }
+
     public List<Project> GenerateProjectCards(){
         //Read text file line by line
         List<Project> myProjects = new List<Project>();
         try{
             var lines = System.IO.File.ReadLines("wwwroot/Texts/projects.txt");
             foreach (var line in lines){
-                Console.WriteLine(line);
+                // Console.WriteLine(line);
                 string[] allWords = line.Split(" ");
                 
                 Enum.TryParse(allWords[1], out Project.Type prjType);
--- obj/Debug/net8.0/MyWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d7570addc7e7cf9aa28fe0d38db531e6b9f4865")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5ef68ca1b34e9cc8099a007349a0a7d60190d82")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
