4a2404e75efc421d51043d3b62673394aaeedd37
Soroor Sadat Seyed Aghamiri <aghamiri.soroor@gmail.com>
2024-07-16 11:29:19 -0400

    Updated favicon

--- obj/Debug/net8.0/MyWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456ffc7a6065408b922420004f45a62d3b1d8ba3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe9296778d5794cc443660c4926539898318c6de")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
